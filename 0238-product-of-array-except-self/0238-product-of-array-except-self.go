449da093ed3eb8f34922cff9f0e3257c6be2e6c8
Bete Goshme <99134122+bete7512@users.noreply.github.com>
2025-09-26 13:38:42 +0300

    Time: 8 ms (14.84%), Space: 10.3 MB (52.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -3,38 +3,25 @@ func productExceptSelf(nums []int) []int {
 
 	prefixes := make([]int, len(nums))
 	postfixes := make([]int, len(nums))
-	for i := 0; i < len(nums);{
+	for i := 0; i < len(nums); {
 		if i == 0 {
 			prefixes[i] = 1
 		}
 		if i > 0 {
-			prefixes[i] = prefixes[i - 1] * nums[i - 1]
+			prefixes[i] = prefixes[i-1] * nums[i-1]
 		}
-		i++
 
-	}
-	for i := 0; i<len(nums);{
 		j := len(nums) - 1 - i
-		if j == len(nums) - 1 {
+		if j == len(nums)-1 {
 			postfixes[j] = 1
 		}
-		if j < len(nums) - 1 {
-			postfixes[j] = postfixes[j + 1] * nums[j + 1]
+		if j < len(nums)-1 {
+			postfixes[j] = postfixes[j+1] * nums[j+1]
 		}
 		i++
+
 	}
 	for i := 0; i < len(nums); {
-		// prefix := 1
-		// postfix := 1
-		// for j := i + 1; j < len(nums); {
-		// 	postfix *= nums[j]
-		// 	j++
-		// }
-
-		// for k := 0; k < i; {
-		// 	prefix *= nums[k]
-		// 	k++
-		// }
 
 		results[i] = prefixes[i] * postfixes[i]
 		i++
