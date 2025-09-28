c7009483849939d11b68f0d72173e9c666056dac
Bete Goshme <99134122+bete7512@users.noreply.github.com>
2025-09-28 15:27:17 +0300

    Time: 54 ms (45.54%), Space: 14 MB (16.14%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
@@ -4,26 +4,55 @@ func longestConsecutive(nums []int) int {
 	for i := 0; i < len(nums); i++ {
 		maps[nums[i]] = i
 	}
-
-	for num := range maps{
-		if _, ok := maps[num-1]; ok {
+	for num := range maps {
+        if _, ok := maps[num-1]; ok {
 			continue
 		}
-
 		countConsecutive := 1
-		k := num + 1
-		for {
-			_, ok := maps[k]
-			if !ok {
-				break
-			}
+		if _, ok := maps[num+1]; ok {
 			countConsecutive++
-			k++
+			k := num + 2
+			for {
+				_, ok := maps[k]
+				if !ok {
+					break
+				}
+				countConsecutive++
+				k++
+			}
 		}
-
 		if countConsecutive > largestConsecutiveCount {
 			largestConsecutiveCount = countConsecutive
 		}
 	}
 	return largestConsecutiveCount
 }
+// func longestConsecutive(nums []int) int {
+// 	maps := make(map[int]int)
+// 	largestConsecutiveCount := 0
+// 	for i := 0; i < len(nums); i++ {
+// 		maps[nums[i]] = i
+// 	}
+
+// 	for num := range maps{
+// 		if _, ok := maps[num-1]; ok {
+// 			continue
+// 		}
+
+// 		countConsecutive := 1
+// 		k := num + 1
+// 		for {
+// 			_, ok := maps[k]
+// 			if !ok {
+// 				break
+// 			}
+// 			countConsecutive++
+// 			k++
+// 		}
+
+// 		if countConsecutive > largestConsecutiveCount {
+// 			largestConsecutiveCount = countConsecutive
+// 		}
+// 	}
+// 	return largestConsecutiveCount
+// }
